d5b4e638fd140ca0ba4b83c60c9ef25774ab256b
Zakhar Starokozhev <zacharysato@hotmail.com>
2021-03-25 02:06:39 +0300

    #2 Fixed exception type and custom lambda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +11,7 @@ public class Main {
         int number;
         try {
             number = input.nextInt();
-        } catch (NumberFormatException e) {
+        } catch (InputMismatchException e) {
             System.out.println("Ввели что-то не то, будет ноль.");
             number = 0;
         }
@@ -33,25 +34,19 @@ public class Main {
                     }
                 }))
         );
-        //индекс увеличивается на свое значение модификатор раз
+        //модификатор суммируется индекс раз
         System.out.println(Arrays.toString(
                 getFilledArray(SIZE, number, (x, y) -> {
-                    for (int i = 0; i < y; i++) {
-                        x += x;
-                    }
-                    return x;
-                }))
-        );
-        //если индекс меньше модификатора - сумма, если больше - разность, при равенстве - сам модификатор
-        System.out.println(Arrays.toString(
-                getFilledArray(SIZE, number, (x, y) -> {
-                    if (x < y) {
-                        return x + y;
-                    }
-                    if (x > y) {
-                        return x - y;
+                    for (int i = 0; i < x; i++) {
+                        y += y;
                     }
                     return y;
+                }))
+        );
+        //разность произведения и суммы
+        System.out.println(Arrays.toString(
+                getFilledArray(SIZE, number, (x, y) -> {
+                    return x * y - (x + y);
                 }))
         );
 
